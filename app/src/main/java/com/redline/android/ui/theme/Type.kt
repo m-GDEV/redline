e895498869c91772e248233cb93dd3ad1f9d5890
Talha Malik <tmalik04@uoguelph.ca>
2024-01-19 16:14:08 -0500

    Changed global font to Outfit

--- app/src/main/java/com/redline/android/ui/theme/Type.kt
+++ app/src/main/java/com/redline/android/ui/theme/Type.kt
@@ -2,33 +2,32 @@ package com.redline.android.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.redline.android.R
+
+val Outfit = FontFamily(
+    Font(R.font.outfit_regular),
+    Font(R.font.outfit_medium, FontWeight.Medium),
+    Font(R.font.outfit_semibold, FontWeight.Bold)
+)
+
+
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Outfit,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    // Other default text styles to override
+
+    //Add text styles for medium and bold
+
 )
